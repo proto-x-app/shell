e15d5f5ec01ec0ed3c1aa4e3c14edef288cad118
pbicskei <53083249+pbicskei@users.noreply.github.com>
2024-01-27 15:16:28 +0000

    Update x.py, make message more generic.

--- x.py
+++ x.py
@@ -112,8 +112,8 @@ shell = get_shell()
 
 # Example command usage (uncomment to use):
 if isinstance(shell, WindowsShell):
-    shell.powershell_command('Write-Host "Hello from `hostname` on Windows"')
+    shell.powershell_command('Write-Host "Hello from Windows on `hostname`, $USER"')
 elif isinstance(shell, LinuxShell):
-    shell.execute_command('echo "Hello from `hostname` on Linux"')  # Corrected method call
+    shell.execute_command('echo "Hello from Linux on `hostname`, $USER"')
 elif isinstance(shell, MacShell):
-    shell.zsh_command('echo "Hello from `hostname` on MacOs"')
+    shell.zsh_command('echo "Hello from MacOs on `hostname`, $USER"')
